3c223e8f6f484408f3f99d485e08ff9a9dc7a143
yun <yunsukchang@gmail.com>
2015-10-06 22:23:32 -0700

    Added ARMemo

---
+++ RulerApp/Assets/ARMemo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARMemo : MonoBehaviour {
+
+	/// <summary>
+	/// The animation playing.
+	/// </summary>
+	private Animation m_anim;
+	
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	private void Start()
+	{
+		m_anim = GetComponent<Animation>();
+		m_anim.Play("ShowMemo", PlayMode.StopAll);
+	}
+	
+	/// <summary>
+	/// Plays an animation, then destroys.
+	/// </summary>
+	private void Hide()
+	{
+		m_anim.Play("HideMemo", PlayMode.StopAll);
+	}
+	
+	/// <summary>
+	/// Callback for the animation system.
+	/// </summary>
+	private void HideDone()
+	{
+		Destroy(gameObject);
+	}
+
+}
